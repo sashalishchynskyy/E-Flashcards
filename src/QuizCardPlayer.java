5f8a007a66263554395c58a14e4acc517c3980ea
Sasha Lishchynskyy <sasha.lishchynskyy@gmail.com>
2017-05-02 08:58:29 +0300

    Created QuizCardPlayer class
    A playback engine that can load a flashcard set and play it for the
    user.

--- src/QuizCardPlayer.java
+++ src/QuizCardPlayer.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 import java.util.ArrayList;
 
 /**
@@ -16,7 +18,7 @@ public class QuizCardPlayer {
     private JButton nextButton;
     private boolean isShowAnswer;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         QuizCardPlayer reader = new QuizCardPlayer();
         reader.go();
     }
@@ -26,7 +28,7 @@ public class QuizCardPlayer {
         JPanel mainPanel = new JPanel();
         Font bigFont = new Font("sanserif", Font.BOLD, 24);
 
-        display = new JTextArea(10,20);
+        display = new JTextArea(10, 20);
         display.setFont(bigFont);
 
         display.setLineWrap(true);
@@ -48,13 +50,62 @@ public class QuizCardPlayer {
         menuBar.add(fileMenu);
         frame.setJMenuBar(menuBar);
         frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
-        frame.setSize(640,500);
+        frame.setSize(640, 500);
         frame.setVisible(true);
     }
 
     private class NextCardListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            if (isShowAnswer) {
+                display.setText(currentCard.getAnswer());
+                nextButton.setText("Next Card");
+                isShowAnswer = false;
+            } else {
+                if (currentCardIndex < cardList.size()) {
+                    showNextCard();
+                } else {
+                    display.setText("That was last card");
+                    nextButton.setEnabled(false);
+                }
+            }
+        }
+    }
+
+    private void showNextCard() {
+        currentCard = cardList.get(currentCardIndex);
+        currentCardIndex++;
+        display.setText(currentCard.getQuestion());
+        nextButton.setText("Show Answer");
+        isShowAnswer = true;
     }
 
     private class OpenMenuListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev){
+            JFileChooser fileOpen = new JFileChooser();
+            fileOpen.showOpenDialog(frame);
+            loadFile(fileOpen.getSelectedFile());
+        }
+    }
+
+    private void loadFile(File selectedFile) {
+        cardList = new ArrayList<QuizCard>();
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(selectedFile));
+            String line = null;
+            while((line = reader.readLine()) != null){
+                makeCard(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        showNextCard();
+    }
+
+    private void makeCard(String line) {
+        String[] result = line.split("/");
+        QuizCard card = new QuizCard(result[0], result[1]);
+        cardList.add(card);
+        System.out.println("Made a card");
     }
 }
